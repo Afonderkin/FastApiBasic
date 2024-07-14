35f45aad534af4be86ed742b6693466a7928d528
AfonderkinS <afonderkin_stas@mail.ru>
2024-07-14 22:05:28 +0300

    build basic FastApi app

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+# from app.core.config import settings
+# from app.api.endpoints import example
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title=settings.PROJECT_NAME,
+        version=settings.PROJECT_VERSION,
+        description=settings.PROJECT_DESCRIPTION,
+    )
+    # Include routers
+    # app.include_router(example.router, prefix="/api/v1")
+
+    return app
